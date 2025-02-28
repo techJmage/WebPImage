aded15f860ddc4f3933a97b1c68d55b80ca22917
Tech J Image <techjmage@outlook.com>
2025-02-28 10:38:19 +0000

    Add project files.

---
+++ Image.Webp/Convert.cs
@@ -0,0 +1,59 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Versioning;
+
+namespace ImageConversion;
+
+/// <summary>simple and light .netcore service for work with webp images
+/// <para>developed by mojtaba moradi (mojtabamoradi.net@outlook.com)</para>
+/// <para>inspired by Wrapper for WebP format in C#. (GPL) Jose M. Piñeiro</para>
+/// </summary>
+public static class Convert
+{
+    
+    public static void ToWebP(Stream imgStream, string destPath, Size? size = null, int quality = 100)
+    {
+        if (string.IsNullOrEmpty(destPath)) 
+            throw new DirectoryNotFoundException();
+        if (quality <= 0 || quality > 100) { quality = 100; }
+        using WebP webp = new();
+        Bitmap bmp = new(imgStream);
+        if (size.HasValue)
+        {
+            var webp_byte = WebP.EncodeLossless(bmp);
+            bmp = WebP.GetThumbnailQuality(webp_byte, size.Value.Width, size.Value.Height);
+        }
+        WebP.Save(bmp, destPath, quality);
+        bmp.Dispose();
+    }
+    
+    public static byte[] ToWebPByte(Stream imgStream, Size? size = null, int quality = 100)
+    {
+        if (quality <= 0 || quality > 100) { quality = 100; }
+        using WebP webp = new();
+        Bitmap bmp = new(imgStream);
+        if (size.HasValue)
+        {
+            var webp_byte = WebP.EncodeLossless(bmp);
+            bmp = WebP.GetThumbnailQuality(webp_byte, size.Value.Width, size.Value.Height);
+        }
+        var data = WebP.EncodeLossy(bmp, quality);
+        bmp.Dispose();
+        return data;
+    }
+    
+    public static void Resize(Stream imgStream, string destPath, int width, int height, ImageFormat? format = null, bool compress = false)
+    {
+        if (string.IsNullOrEmpty(destPath) || width <= 0 || height <= 0)
+            throw new DirectoryNotFoundException();        
+        using WebP webp = new();
+        Bitmap bmp = new(imgStream);
+        var webp_byte = WebP.EncodeLossless(bmp);
+        if (compress) { bmp = WebP.GetThumbnailFast(webp_byte, width, height); }
+        else { bmp = WebP.GetThumbnailQuality(webp_byte, width, height); }
+        bmp.Save(destPath, format ?? ImageFormat.Jpeg);
+        bmp.Dispose();
+    }    
+
+}
+
---
+++ Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using WP = ImageConversion;
+namespace Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            try
+            {
+                string src = @"C:\Users\uie37359\Pictures\Rule Creation UI-Dashboard.drawio.png";
+                string dest = @"C:\Users\uie37359\Pictures\Rule Creation UI-Dashboard.drawio.webp";
+                //WP.Convert.PngToWebP(src, dest, 1072, 745);
+                //convert png image to webp and save on local path
+                using var srcStrm = File.OpenRead(src);
+                WP.Convert.ToWebP(srcStrm, dest);
+                Console.WriteLine("Done");
+                Console.Read();
+
+                //ImageConversion.Convert.PngToWebPFromWeb("https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png", "webp.webp", 100, 50);
+                //convert web png image to webp and save on local path
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
